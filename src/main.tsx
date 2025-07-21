4b9f3577d4d739a7bf5b205005027ec72ed3aa21
henliwu <kawasakininja3021@gmail.com>
2025-07-21 21:29:16 +0800

    chore: 加上 Sentry 監控功能

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import '../index.css';
+import * as Sentry from '@sentry/react';
 import { initializeMSW } from './mocks/config';
+import '../index.css';
 
 // Initialize app with conditional MSW
 initializeMSW().then(() => {
+  Sentry.init({
+    dsn: 'https://1234567890@o1.ingest.sentry.io/1234567890',
+    tracesSampleRate: 1.0,
+  });
   createRoot(document.getElementById('root')!).render(
     <StrictMode>
       <App />
